c6825296af59c9c1cb9560dcb72b31c483c53564
lbdgraev <laurens.degraeve@ugent.be>
2016-03-04 15:29:52 +0100

    stoopid mongo

---
+++ drywall-workbench/test.js
@@ -0,0 +1,9 @@
+use drywall; // or your mongo db name if different
+
+db.admingroups.insert({ _id: 'root', name: 'Root' });
+db.admins.insert({ name: {first: 'Root', last: 'Admin', full: 'Root Admin'}, groups: ['root'] });
+var rootAdmin = db.admins.findOne();
+db.users.save({ username: 'root', isActive: 'yes', email: 'your@email.addy', roles: {admin: rootAdmin._id} });
+var rootUser = db.users.findOne();
+rootAdmin.user = { id: rootUser._id, name: rootUser.username };
+db.admins.save(rootAdmin);

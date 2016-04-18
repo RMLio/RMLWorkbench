7ade0203bb3703cc93ff51d1b6b74693ba7b1359
lbdgraev <laurens.degraeve@ugent.be>
2016-04-18 11:56:57 +0200

    jade acting up

--- drywall-workbench/views/workbench/index.js
+++ drywall-workbench/views/workbench/index.js
@@ -3,7 +3,18 @@
 exports.init = function(req, res){
   
   req.app.db.models.License.find({},function(err, ls) {
-      res.render('workbench/index',{licenses : ls});
+      var licenseCats = {}
+      for (var key in ls) {         
+         if (ls.hasOwnProperty(key)) {
+            var cat = ls[key].category;
+            if(cat in licenseCats){
+                licenseCats[cat].push(ls[key])
+            } else{
+                licenseCats[cat] = [ls[key]]                
+            }
+         }
+      }
+      res.render('workbench/index',{licenses : licenseCats});
   });
 };
 

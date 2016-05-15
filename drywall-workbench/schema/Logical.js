079069e917a0e1c474a766cbcc2353af2078c8e5
lbdgraev <laurens.degraeve@ugent.be>
2016-05-15 18:36:54 +0200

    forgot to add the schema

---
+++ drywall-workbench/schema/Logical.js
@@ -0,0 +1,17 @@
+exports = module.exports = function(app, mongoose) {
+  var logicalSchema = new mongoose.Schema({
+    _id: { type: mongoose.Schema.Types.ObjectId },
+    source: { type: mongoose.Schema.Types.ObjectId, ref: 'Description' },
+    name: { type: String, default: '' },    
+    type: { type: String, enum: ['file', 'DCAT', 'SPARQL', 'API', 'DB']},
+    data : { type: String, default: ''},
+    metadata: { 
+      timeCreated: { type: Date, default: Date.now },
+      license: { type: String, default: 'None' }
+    }
+  });
+  logicalSchema.plugin(require('./plugins/pagedFind'));
+  logicalSchema.index({ data: 1 });
+  logicalSchema.set('autoIndex', (app.get('env') === 'development'));
+  app.db.model('Logical', logicalSchema);
+};
